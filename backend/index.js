1df50bd920de0aed31e4a86eb5f8f5d54d8ee7b2
g-rashmi <rashmi.gupta7018@gmail.com>
2024-06-28 17:34:21 +0530

    add cache

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const NodeCache = require( "node-cache" );
+const myCache = new NodeCache();
 const admin = require("firebase-admin");
 
 const app = express();
@@ -24,13 +25,20 @@ app.use(bodyParser.json());
 
 app.get("/courses", async (req, res) => {
   try {
-    const snapshot = await db.collection("courses").get();
-    const courses = snapshot.docs.map((doc) => ({
+    let courses;
+    if(myCache.has("courses")){
+      courses=JSON.parse(myCache.get("courses")) ; 
+
+    }
+    else{
+      const snapshot = await db.collection("courses").get();
+  courses = snapshot.docs.map((doc) => ({
       _Id: doc.id,
       ...doc.data(),
     }));
-
-    res.status(200).json(courses);
+ myCache.set("courses",JSON.stringify(courses))
+    }
+    return res.status(200).json(courses);
   } catch (error) {
     res.status(500).json({ message: "Error fetching courses", error });
   }
@@ -38,14 +46,24 @@ app.get("/courses", async (req, res) => {
 
 app.get("/courses/:id", async (req, res) => {
   try { 
-     const docRef = db.collection("courses").doc(req.params.id);
-    const docSnapshot = await docRef.get();
+    let data;
+    if(myCache.has("data")){
+      data=JSON.parse(myCache.get("data")) ; 
 
-    if (!docSnapshot.exists) {
-      return res.status(404).json({ message: "Course not found" });
+} 
+else{
+      const docRef = db.collection("courses").doc(req.params.id);
+      const docSnapshot = await docRef.get();
+  
+      if (!docSnapshot.exists) {
+        return res.status(404).json({ message: "Course not found" });
+      }
+   data={_Id: docSnapshot.id, ...docSnapshot.data() };  
+   myCache.set("data",JSON.stringify(data))
+  
     }
-
-    res.status(200).json({ _Id: docSnapshot.id, ...docSnapshot.data() });
+    
+    res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ message: "Error fetching course", error });
   }
